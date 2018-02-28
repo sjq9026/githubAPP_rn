e4d14bd9d91dff10c761958b289e466eba9f0f4e
lantiankongmo1026@gmail.com <lantiankongmo1026@gmail.com>
2018-02-28 17:40:26 +0800

    选项卡界面搭建

--- index.js
+++ index.js
@@ -1,4 +1,120 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry,Image } from 'react-native';
+import React, { Component } from 'react';
+import {StackNavigator,TabNavigator} from 'react-navigation';
 import App from './App';
+import PopularTab from "./pages/tabs/PopularTab";
+import TrendingTab from "./pages/tabs/TrendingTab";
+import FavoritesTab from "./pages/tabs/FavoritesTab";
+import MineTab from "./pages/tabs/MineTab";
+import WelcomePage from "./pages/WelcomePage";
 
-AppRegistry.registerComponent('githubApp', () => App);
+
+
+const tab = TabNavigator(
+    {
+        Popular:{
+            screen:PopularTab,
+            navigationOptions:{
+                tabBarLable:'Popular',
+                header:null,
+                tabBarIcon:({tintColor,focused})=> (
+                    focused
+                        ?
+                        <Image source={require("./imgs/tab_mine_pressed.png")} style={{height:20,width:20}}/>
+                        :
+                        <Image source={require("./imgs/tab_mine_normal.png")} style={{height:20,width:20}}/>
+                ),
+            }
+        },
+
+
+        Trending:{
+            screen:TrendingTab,
+            navigationOptions:{
+                tabBarLable:'Trending',
+                header:null,
+                tabBarIcon:({tintColor,focused})=> (
+                    focused
+                        ?
+                        <Image source={require("./imgs/tab_mine_pressed.png")} style={{height:20,width:20}}/>
+                        :
+                        <Image source={require("./imgs/tab_mine_normal.png")} style={{height:20,width:20}}/>
+                ),
+            }
+        },
+
+        Favorites:{
+            screen:FavoritesTab,
+            navigationOptions:{
+                tabBarLable:'Favorites',
+                header:null,
+                tabBarIcon:({tintColor,focused})=> (
+                    focused
+                        ?
+                        <Image source={require("./imgs/tab_mine_pressed.png")} style={{height:20,width:20}}/>
+                        :
+                        <Image source={require("./imgs/tab_mine_normal.png")} style={{height:20,width:20}}/>
+                ),
+            }
+        },
+
+        Mine:{
+            screen:MineTab,
+            navigationOptions:{
+                tabBarLable:'Mine',
+                header:null,
+                tabBarIcon:({tintColor,focused})=> (
+                    focused
+                        ?
+                        <Image source={require("./imgs/tab_mine_pressed.png")} style={{height:20,width:20}}/>
+                        :
+                        <Image source={require("./imgs/tab_mine_normal.png")} style={{height:20,width:20}}/>
+                ),
+            }
+        },
+    },
+    {
+        animationEnabled: true,
+        tabBarPosition: 'bottom',
+        swipeEnabled: false,
+        backBehavior: 'none',
+        tabBarOptions: {
+            activeTintColor: 'blue',
+            inactiveTintColor: 'gray',
+            showIcon:true,//是否显示图标
+            showLabel:true,//是否显示文字
+            indicatorStyle: {
+                height: 0,
+            },
+            style:{
+                backgroundColor:'transparent',
+                height:60,
+            },
+            pressColor: 'gray',
+            pressOpacity: 0.8,
+            upperCaseLabel: false,
+        },
+
+    }
+)
+
+const  pages = StackNavigator(
+    {
+
+        Welcome: {
+            screen: WelcomePage,
+            navigationOptions: {
+                header: null,
+            }
+        },
+        Tab: {
+            screen: tab
+        }
+    }
+
+)
+
+
+
+
+AppRegistry.registerComponent('githubApp', () => pages);
