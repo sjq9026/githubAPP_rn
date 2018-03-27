3c01c04f6859d9eb7f813c6219cfaa10f66847e2
lantiankongmo1026@gmail.com <lantiankongmo1026@gmail.com>
2018-03-27 10:11:02 +0800

    删除popover组件相关代码

--- pages/tabs/TrendingTab.js
+++ pages/tabs/TrendingTab.js
@@ -14,7 +14,6 @@ import {
     TouchableOpacity,
     TouchableHighlight
 } from 'react-native';
-import  Popover from  "../other/Popover";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -24,16 +23,10 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-var spinnerTextArray = ['深圳南山', '深圳宝安', '深圳罗湖', '深圳福田']
 export default class TrendingTab extends Component<Props> {
     constructor(props) {
         super(props);
-        this.state = {
-            //下拉列表是否可见
-            isVisible: false,
-            //下拉列表大小范围
-            spinnerRect: {},
-        }
+
     }
   render() {
     return (
@@ -49,30 +42,7 @@ export default class TrendingTab extends Component<Props> {
 
           </TouchableOpacity>
 
-          <Popover
-              //设置可见性
-              isVisible={this.state.isVisible}
-              //设置下拉位置
-              fromRect={this.state.spinnerRect}
-              placement="bottom"
-              //点击下拉框外范围关闭下拉框
-              onClose={()=>this.closeSpinner()}
-              //设置内容样式
-              contentStyle={{opacity:0.82,backgroundColor:'#343434'}}
-              style={{backgroundColor: 'red'}}>
-              <View style={{alignItems: 'center'}}>
-                  {spinnerTextArray.map((result, i, arr) => {
-                      return <TouchableHighlight key={i} onPress={()=>this.onItemClick(arr[i])}
-                                                 underlayColor='transparent'>
-                          <Text
-                              style={{fontSize: 18,color:'white', padding: 8, fontWeight: '400'}}>
-                              {arr[i]}
-                          </Text>
-                      </TouchableHighlight>
-                  })
-                  }
-              </View>
-          </Popover>
+
 
 
       </View>
@@ -82,28 +52,6 @@ export default class TrendingTab extends Component<Props> {
 
   }
 
-    //显示下拉列表
-    showSpinner() {
-        this.refs.spinner.measure((ox, oy, width, height, px, py) => {
-            this.setState({
-                isVisible: true,
-                spinnerRect: {x: px, y: py, width: width, height: height}
-            });
-        });
-    }
-
-    //隐藏下拉列表
-    closeSpinner() {
-        this.setState({
-            isVisible: false
-        });
-    }
-
-    //下拉列表每一行点击事件
-    onItemClick(spinnerItem) {
-        this.closeSpinner();
-        this.toast.show(spinnerItem, DURATION.LENGTH_SHORT);
-    }
 
 }
 
